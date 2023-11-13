a9fbe3e17ed3e8fc809c253768e1c415bf89211a
Muhammad Nawzad <hama127n@gmail.com>
2023-11-13 15:30:06 +0300

    Updates Schemable Module

--- lib/schemable.rb
+++ lib/schemable.rb
@@ -1,13 +1,11 @@
-# frozen_string_literal: true
-
 require_relative 'schemable/version'
 require_relative 'schemable/definition'
 require_relative 'schemable/configuration'
 require_relative 'schemable/schema_modifier'
 require_relative 'schemable/attribute_schema_generator'
-require_relative 'schemable/response_schema_generator'
 require_relative 'schemable/relationship_schema_generator'
 require_relative 'schemable/included_schema_generator'
+require_relative 'schemable/response_schema_generator'
 require_relative 'schemable/request_schema_generator'
 
 module Schemable
@@ -19,19 +17,6 @@ module Schemable
     def configure
       @configuration ||= Configuration.new
       yield(@configuration) if block_given?
-    end
-
-    def generate_schemas
-      klasses = Schemable::Definition.descendants
-      generated_schemas = []
-
-      klasses.each do |klass|
-        model_definition = klass.new
-        schema = AttributeSchemaGenerator.new(model_definition).generate
-        generated_schemas << schema
-      end
-
-      generated_schemas
     end
   end
 end
